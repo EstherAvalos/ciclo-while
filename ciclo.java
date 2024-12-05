b2c19f81f05ddc023fd630a9a4d159d58d23aec8
EstherAvalos <teteavalosgarcia2@gmail.com>
2024-12-04 20:50:38 -0600

    Initial commit

---
+++ ciclo.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class ciclo {
+
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        int i=1, contador;
+        System.out.println("Digite cuantos numeros quiere en pantalla: ");
+        contador = entrada.nextInt();
+        while (i<=contador) {
+            System.out.println(i);
+            i+=2;
+        }
+    }
+}
